e9f4351293b6b708993513075f7429ba5e7c1d5b
carlos <carlos.marques.miranda@escola.pr.gov.br>
2025-05-06 12:33:50 -0300

    capitulo 2 expressoes e instruçoes

---
+++ pythonProject2/Capitulo 2/exemplo2nomesvariaveis.py
@@ -0,0 +1,7 @@
+  ## variaves nao podem começar com numeros
+      ## 76trombones = 'big parede'
+  ## variavis  nao podem comecar com letras maiusculas
+  # More = 10000
+
+  # variaveis nao podem ter caracteres especiais tirando o _
+  # more@ = 1000
---
+++ pythonProject2/Capitulo 2/exemplo3expressoesinstrucoes.py
@@ -0,0 +1,6 @@
+n = 17
+n = 25
+
+total = n + n
+
+print(total)
