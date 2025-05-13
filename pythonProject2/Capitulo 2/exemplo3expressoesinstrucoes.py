a8e66c258cec96d084196bdbd6035fbd9f1656e9
carlos <carlos.marques.miranda@escola.pr.gov.br>
2025-05-13 11:31:36 -0300

    capitulo1 exemplo1

--- pythonProject2/Capitulo 2/exemplo3expressoesinstrucoes.py
+++ pythonProject2/Capitulo 2/exemplo3expressoesinstrucoes.py
@@ -2,7 +2,7 @@ n = 17
 n = 25
  #isto é um expresao
  #expressões podem ser a conbinação de variaveis.
- #valores e  operdores
+ #valores e  operadores
 total = n + n
 
 print(total)
---
+++ pythonProject2/exemplo1.py
@@ -0,0 +1,14 @@
+# operador que soma
+soma = 1 + 2
+
+# operador de subritacão
+subritacao = 2 - 1
+
+# operador de multiplicacao
+multiplicacao = 2 * 1
+
+# operador de divisao
+divsao = 2 / 1
+
+# operador de exponenciacao
+exponenciacao = 2 ** 1
