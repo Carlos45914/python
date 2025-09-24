6b6111fa078bd12950104324addc532d3b382847
maria ribeiro <maria.gil.oliveira@escola.pr.gov.br>
2025-09-24 11:04:15 -0300

    exercicio1 e exercicio2

---
+++ capitulo 5/exercicio 1.py
@@ -0,0 +1,9 @@
+#exrcicio 1
+def soma(numero,numero1):
+    return print(f'A soma dos numeros è: {numero + numero1}')
+
+numero = int(input("digite o primeiro numero interio"))
+numero1 = int(input("digite o segundo numero inteiro "))
+
+soma(numero,numero1)
+
---
+++ capitulo 5/exercicio2.py
@@ -0,0 +1,15 @@
+#exercicio 2
+
+
+
+def resultadoFinal(numerosSomados, valor):
+    return print(f"somando todos o numeros e depois subtraia por 10 o resultado è {numerosSomados - valor}")
+
+numero = int(input('digite o primeiro numero inteiro'))
+numero1 = int(input('digite o segundo numero inteiro'))
+numero2 = int(input('digite o terceiro numero inteiro'))
+numero3 = int(input('digite o quarto numero inteiro'))
+
+numerosSomados = (numero + numero1 + numero2 + numero3)
+valor = 10
+resultadoFinal=(numerosSomados,valor)
